65f74d6d074825d7c2f740297ab9e3a3d811c6f8
Geon George <geongeorge98@gmail.com>
2019-12-01 08:50:17 +0530

    build sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,62 @@
 <template>
+<div>
   <div>
-    <nuxt />
+        <div class="float-right z-30 fixed top-0 right-0 m-1 pt-4">
+            <div class="z-30 lg:hidden">
+    <button 
+    v-if="!sidebar"
+    @click="sidebar=true"
+    type="button" id="sidebar-open" class="flex px-6 items-center  text-gray-700 focus:outline-none focus:text-gray-800">
+           menu
+    </button>
+    <button
+    v-else
+      type="button"
+      id="sidebar-close"
+       @click="sidebar=false"
+      class="flex px-6 items-center text-gray-700 focus:outline-none focus:text-gray-800"
+    >
+      close
+    </button>
+  </div>
+        </div>
+        <div class="clearfix"></div>
+      
+    </div>
+  <div class="min-h-screen lg:flex">
+    <no-ssr>
+    <div class="static h-full inset-0 h-full bg-gray-100 z-20 flex-none w-full lg:w-1/5 bg-gray-200 text-gray-700"
+    :class="{'hidden lg:block': !sidebar}">
+    <div class="fixed bg-gray-200 h-screen w-full lg:w-1/5">
+    
+        <div class='px-2'>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, tempore?
+          </p>
+        </div>
+        </div>
+    </div>
+      </no-ssr>
+    <div class="flex-1 bg-white lg:w-4/5 lg:static text-gray-700">
+        <div class="container mx-auto mt-5 lg:mt-20 px-2 text-left">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minus quia nostrum id. Odit consectetur aliquid, doloremque eaque ducimus nostrum eligendi minima ratione blanditiis tempore ipsam exercitationem ex et repellendus quasi minus reprehenderit officia ea sed beatae! Harum, sit neque.
+            </p>
+        </div>
+        <!-- footer -->
+    </div>
+    </div>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      sidebar : false
+    }
+  }
+}
+</script>
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,8 @@
 <template>
-  <div class="container bg-gray-800 h-screen">
-    <!-- 2 divs with w-1/2 and on large screens it will be w-1/5 and w-4/5  -->
-    <div class="flex">
-      <div class="w-1/2 lg:w-1/5 p-2">
-        <div class="bg-gray-600 rounded h-32 w-full"></div>
-      </div>
-      <div class="w-1/2 lg:w-4/5 p-2">
-       <div class="bg-gray-600 rounded h-32 w-full"></div>
-      </div>
-    </div>
-     
+  <div class="text-white text-center">
+     <p>
+       Hello
+     </p>
   </div>
 </template>
 
